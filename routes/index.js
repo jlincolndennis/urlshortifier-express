7a35db26fd4034c1f84828581a7f54ef82723511
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2017-01-18 16:31:49 -0700

    tokens generated

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lincoln\'s Url Shortifier!' });
 });
 
+router.get('/success', function (req, res, next) {
+  res.render('success', {token: req.query.token})
+})
+
+
+router.post('/submit', function (req, res, next) {
+  let links = {}
+  links.target_url = req.body.target_url
+  links.token = randomString(12)
+  console.log(links);
+  res.redirect(`/success?token=${links.token}`);
+
+
+
+})
 module.exports = router;
+
+
+
+function randomString(length) {
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
